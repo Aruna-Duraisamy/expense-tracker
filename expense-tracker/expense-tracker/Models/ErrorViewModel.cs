8d0bed9d960f52a2de2163aa8b34b816265c21a7
arunaganeshraj <aruna.durai@gmail.com>
2021-10-21 23:00:57 +0100

    Initial Commit

---
+++ expense-tracker/expense-tracker/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+﻿using System;
+
+namespace expense_tracker.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
