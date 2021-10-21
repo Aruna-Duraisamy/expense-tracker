5dc57aad7989bdcee360011f2325ccbc5e370fa1
arunaganeshraj <aruna.durai@gmail.com>
2021-10-21 23:15:43 +0100

    Include new expense page

--- expense-tracker/expense-tracker/Controllers/HomeController.cs
+++ expense-tracker/expense-tracker/Controllers/HomeController.cs
@@ -28,6 +28,11 @@ namespace expense_tracker.Controllers
             return View();
         }
 
+        public IActionResult Expenses()
+        {
+            return View();
+        }
+
         [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
         public IActionResult Error()
         {
