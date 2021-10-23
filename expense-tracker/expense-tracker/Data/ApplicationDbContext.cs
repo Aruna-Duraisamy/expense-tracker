26caf1dd155b15577d18c1cff681b5088c86a207
arunaganeshraj <aruna.durai@gmail.com>
2021-10-23 06:00:15 +0100

    Added DbContext and Model

---
+++ expense-tracker/expense-tracker/Data/ApplicationDbContext.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using expense_tracker.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace expense_tracker.Data
+{
+    public class ApplicationDbContext:DbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options):
+            base(options)
+        {
+        }
+
+        public DbSet<Expense> Expenses { get; set; }
+    }
+
+
+}
---
+++ expense-tracker/expense-tracker/Models/Expense.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+
+namespace expense_tracker.Models
+{
+    public class Expense
+    {
+        public Expense()
+        {
+        }
+
+        [Key]
+        public int Id { get; set; }
+        public string ExpenseName { get; set; }
+        public decimal ExpenseAmount { get; set; }
+        public DateTime ExpenseDate { get; set; }
+        //TODO: Expense Category, SubCategory (Eg: HomeExpense - Internet)
+    }
+}
